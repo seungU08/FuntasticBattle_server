fd39307341bbdd8e17cac258638bcd37d7d85e93
seungU08 <sungwoo5957@naver.com>
2026-04-13 23:21:12 +0900

    기존작업중이던 서버

---
+++ FBServer/GameServer/GameServer.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+
+#include <iostream>
+#include "CorePch.h"
+#include "ThreadManager.h"
+
+
+
+int main()
+{
+	for (int32 i = 0; i < 5; ++i) {
+		GthreadManager->Launch(ThreadMain);
+	}
+
+	GthreadManager->Join();
+}
---
+++ FBServer/ServerCore/CoreGlobal.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern class ThreadManager* GthreadManager;
+
+extern class DeadLockProfiler* GDeadLockProfiler;
+
